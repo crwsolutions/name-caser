877e173b592f8a8182de5df82f24165254c1886b
Colin Wilmans <colin.wilmans@crwsolutions.nl>
2024-05-29 10:37:59 +0200

    Added PascalCasing(method)

---
+++ src/NameCaser/ToPascalCaseExtension.cs
@@ -0,0 +1,43 @@
+﻿using static System.Globalization.UnicodeCategory;
+
+namespace NameCaser;
+
+public static class ToPascalCaseExtension
+{
+
+    /// <summary>
+    /// Phrase to PascalCase, eg. '- Some UTP cable' to SomeUTPCable
+    /// </summary>
+    public static string ToPascalCase(this string phrase)
+    {
+        if (string.IsNullOrEmpty(phrase))
+        {
+            return phrase;
+        }
+
+        var chars = phrase.AsSpan();
+        var length = 0;
+        var isSeparated = true;
+        UnicodeCategory c;
+        Span<char> span = new char[chars.Length];
+        var i = 0;
+        do
+        {
+            c = char.GetUnicodeCategory(chars[i]);
+            if (c is not LowercaseLetter && c is not DecimalDigitNumber)
+            {
+                isSeparated = true;
+            }
+
+            if (c is LowercaseLetter || c is UppercaseLetter || c is DecimalDigitNumber)
+            {
+                span[length] = isSeparated ? char.ToUpperInvariant(chars[i]) : char.ToLowerInvariant(chars[i]);
+                isSeparated = false;
+                length++;
+            }
+            i++;
+        } while (i < chars.Length);
+
+        return span.Slice(0, length).ToString();
+    }
+}
