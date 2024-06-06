d0f5ba24a826f35bf8959f302f097a5ef7697375
Colin Wilmans <colin.wilmans@crwsolutions.nl>
2024-06-06 21:02:55 +0200

    Added a Stackalloc benchmark

---
+++ src/NameCaserBenchmark/BenchMarksStackalloc.cs
@@ -0,0 +1,108 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace NameCaserBenchmark;
+
+//| Method            | Mean     | Error    | StdDev   | Ratio | RatioSD | Gen0   | Allocated | Alloc Ratio |
+//|------------------ |---------:|---------:|---------:|------:|--------:|-------:|----------:|------------:|
+//| AnalyzeBase       | 30.06 ns | 0.608 ns | 0.747 ns |  1.00 |    0.00 | 0.0057 |      48 B |        1.00 |
+//| AnalyzeStackalloc | 33.11 ns | 0.298 ns | 0.279 ns |  1.10 |    0.03 |      - |         - |        0.00 |
+
+[MemoryDiagnoser(true)]
+public class BenchMarksStackalloc
+{
+    readonly string _pascalCase = "IODeviceSomeLongerString";
+
+    [Benchmark(Baseline = true)]
+    public Types AnalyzeBase()
+    {
+        return AnalyzeBaseLine(_pascalCase);
+    }
+
+    [Benchmark]
+    public Types AnalyzeStackalloc()
+    {
+        return AnalyzeStackalloc(_pascalCase);
+    }
+
+    [Flags]
+    public enum Types : byte
+    {
+        Lower = 0,              // 0000 0000
+        Upper = 1 << 0,         // 0000 0001
+        Break = 1 << 1,         // 0000 0010
+        Abbreviation = 1 << 2,  // 0000 0100
+        First = 1 << 3          // 0000 1000
+    }
+
+    public static Types AnalyzeBaseLine(ReadOnlySpan<char> chars)
+    {
+        var types = new Types[chars.Length];
+        var breaks = 0;
+        var last = Types.Break;
+        for (var i = 0; i < chars.Length; i++)
+        {
+            if (char.IsUpper(chars[i]))
+            {
+                if (last == Types.Lower)
+                {
+                    types[i] = Types.Break;
+                    breaks++;
+                    last = Types.Break;
+                }
+                else
+                {
+                    types[i] = Types.Upper;
+                    last = Types.Upper;
+                }
+            }
+            else
+            {
+                if (last == Types.Upper && i > 1)
+                {
+                    types[i - 1] = Types.Break;
+                    breaks++;
+                }
+
+                last = Types.Lower;
+            }
+        }
+
+        return last;
+    }
+
+    public static Types AnalyzeStackalloc(ReadOnlySpan<char> chars)
+    {
+        Span<Types> types = stackalloc Types[chars.Length];
+        var breaks = 0;
+        var last = Types.Break;
+        for (var i = 0; i < chars.Length; i++)
+        {
+            if (char.IsUpper(chars[i]))
+            {
+                if (last == Types.Lower)
+                {
+                    types[i] = Types.Break;
+                    breaks++;
+                    last = Types.Break;
+                }
+                else
+                {
+                    types[i] = Types.Upper;
+                    last = Types.Upper;
+                }
+            }
+            else
+            {
+                if (last == Types.Upper && i > 1)
+                {
+                    types[i - 1] = Types.Break;
+                    breaks++;
+                }
+
+                last = Types.Lower;
+            }
+        }
+
+        return last;
+    }
+}
--- src/NameCaserBenchmark/BenchMarksTrainCasing.cs
+++ src/NameCaserBenchmark/BenchMarksTrainCasing.cs
@@ -4,10 +4,33 @@ using System.Text;
 
 namespace NameCaserBenchmark;
 
-//| Method         | Mean     | Error   | StdDev  | Ratio | Gen0   | Allocated | Alloc Ratio |
-//|--------------- |---------:|--------:|--------:|------:|-------:|----------:|------------:|
-//| CamelCaseOrig  | 146.8 ns | 1.98 ns | 1.76 ns |  1.00 | 0.0918 |     288 B |        1.00 |
-//| CamelCaseFinal | 113.4 ns | 1.49 ns | 1.40 ns |  0.77 | 0.0663 |     208 B |        0.72 |
+//Before:
+
+//| Method         | Mean     | Error    | StdDev   | Ratio | Gen0   | Allocated | Alloc Ratio |
+//|--------------- |---------:|---------:|---------:|------:|-------:|----------:|------------:|
+//| CamelCaseOrig  | 94.72 ns | 1.631 ns | 1.526 ns |  1.00 | 0.0343 |     288 B |        1.00 |
+//| CamelCaseFinal | 72.78 ns | 0.937 ns | 0.783 ns |  0.77 | 0.0248 |     208 B |        0.72 |
+
+//Inline:
+
+//| Method         | Mean     | Error    | StdDev   | Ratio | RatioSD | Gen0   | Allocated | Alloc Ratio |
+//|--------------- |---------:|---------:|---------:|------:|--------:|-------:|----------:|------------:|
+//| TrainCaseOrig  | 93.56 ns | 1.880 ns | 1.758 ns |  1.00 |    0.00 | 0.0343 |     288 B |        1.00 |
+//| TrainCaseFinal | 79.89 ns | 0.989 ns | 0.925 ns |  0.85 |    0.02 | 0.0248 |     208 B |        0.72 |
+
+//Stackalloc:
+
+//| Method         | Mean      | Error    | StdDev   | Ratio | RatioSD | Gen0   | Allocated | Alloc Ratio |
+//|--------------- |----------:|---------:|---------:|------:|--------:|-------:|----------:|------------:|
+//| TrainCaseOrig  |  93.10 ns | 1.259 ns | 1.178 ns |  1.00 |    0.00 | 0.0343 |     288 B |        1.00 |
+//| TrainCaseFinal | 115.22 ns | 1.401 ns | 1.170 ns |  1.24 |    0.03 | 0.0191 |     160 B |        0.56 |
+
+//String.Create:
+
+//| Method         | Mean     | Error    | StdDev   | Ratio | Gen0   | Allocated | Alloc Ratio |
+//|--------------- |---------:|---------:|---------:|------:|-------:|----------:|------------:|
+//| TrainCaseOrig  | 98.91 ns | 0.961 ns | 0.899 ns |  1.00 | 0.0343 |     288 B |        1.00 |
+//| TrainCaseFinal | 75.61 ns | 1.053 ns | 0.985 ns |  0.76 | 0.0267 |     224 B |        0.78 |
 
 [MemoryDiagnoser(true)]
 public class BenchMarksTrainCasing
@@ -15,7 +38,7 @@ public class BenchMarksTrainCasing
     readonly string _pascalCase = "IODeviceSomeLongerString";
 
     [Benchmark(Baseline = true)]
-    public string? CamelCaseOrig()
+    public string? TrainCaseOrig()
     {
         if (_pascalCase is null) return null;
 
@@ -61,7 +84,7 @@ public class BenchMarksTrainCasing
     }
 
     [Benchmark]
-    public string CamelCaseFinal()
+    public string TrainCaseFinal()
     {
         return _pascalCase.ToTrainCase()!;
     }
